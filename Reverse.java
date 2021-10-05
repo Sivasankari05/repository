df97e55b4894dad0e721ad3907c63e23cd4c8aac
Sivasankari05 <91413505+Sivasankari05@users.noreply.github.com>
2021-10-05 16:27:07 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,23 @@
+import java.until.*;
+public class   Reverse
+{
+	public static void main(String[] args) 
+	{
+		Scanner in=new  Scanner(System.in);
+		System.out.println("Enter the size of the array");
+		int n=in.nexInt();
+		int[]elt=new.int[n];
+		for(int i=0;i<n;i++)
+		{
+       System.out.println("Enter array element "+(i+1));
+	   elt[i]=sc.nextInt();
+	}
+ System.out.println("original element are:");
+ for(int i=0;i<n;i++)
+	{
+System.out.println(elt[i]+ " ");
+System.out.println("Enter reverse element are:")
+	for(int i=n-1;i>=0;i--)
+		{
+	System.out.println(elt[i]+ " ");
+}
