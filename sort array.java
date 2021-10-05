3099926c3127edac113a67932ed35a2b8a9bc61a
Sivasankari05 <91413505+Sivasankari05@users.noreply.github.com>
2021-10-05 16:29:26 +0530

    Add files via upload

---
+++ sort array.java
@@ -0,0 +1,14 @@
+import java.until.*;
+ public class   sort array
+{
+	public static void main(String[] args) 
+	{
+		int[]array = new int[]{90,23,5,109,12,67,34};
+		Array.sort(array);
+		System.out.println("Elements of array sorted in ascending order:");
+	for (int i=0;i<array.length;i++; )
+	{
+	System.out.println(array[i]);
+	}
+}
+}
